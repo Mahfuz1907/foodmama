4bea29fe437dfd35046ea2122b1eef694698b77f
MD. Mahfuzul Alam <121704002+Mahfuz1907@users.noreply.github.com>
2024-12-22 17:48:23 +0600

    Update index.js

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
+import { IoIosRestaurant } from "react-icons/io";
 
 const Navigation = () => {
   return (
     <>
     <nav className="nav">
     <ul class="list-inline list-inline-horizontal">
-  <li class="list-inline-item ms-auto"><Link to={'/'}><button>Home</button></Link></li>
-  <li class="list-inline-item ms-auto"><Link to={'/rest'}><button>Restaurants</button></Link></li>
+  <li class="list-inline-item ms-auto"><Link to={'/'}><button>Home<FaHome className='ms-2'/></button></Link></li>
+  <li class="list-inline-item ms-auto"><Link to={'/rest'}><button>Restaurants<IoIosRestaurant className='ms-2'/></button></Link></li>
 </ul>
 
     </nav>
